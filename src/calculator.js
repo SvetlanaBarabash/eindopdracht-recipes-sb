60c42eb13bca9a03ac0a0a677faf9e3ececf0ffc
SvetlanaBarabash <svetlana.barabash17@gmail.com>
2022-12-09 22:29:13 +0100

    Calculator page is ready.

--- src/calculator.js
+++ src/calculator.js
@@ -1,27 +1,35 @@
 import fetchFoodData from "./functions/fetchFoodData";
 
+let hit;
+let foodTableBody;
+let foodLabel;
+let searchBarcode;
+
+
+
 window.addEventListener("load", () => {
     const searchBarcodeBtn = document.getElementById("search-barcode");
+
     searchBarcodeBtn.addEventListener('click', (e) => {
         // Prevent form from auto-submitting
         e.preventDefault();
 
-        const searchBarcode = document.getElementById('barcode-product').value;
+        searchBarcode = document.getElementById('barcode-product').value;
 
         fetchFoodData(searchBarcode).then((data) => {
 
             const hits = data.slice(0, 1);
-            for (const hit of hits) {
+            for (hit of hits) {
 
-                const foodLabel = hit.food.label;
-                const quantity = Math.round(hit.measures[0].weight);
-                const label = hit.measures[0].label;
+                foodLabel = hit.food.label;
+                let quantity = Math.round(hit.measures[0].weight);
+                let label = hit.measures[0].label;
 
                 console.log(foodLabel);
                 console.log(quantity);
                 console.log(label);
 
-                const foodTableBody = document.getElementById('foodTableBody');
+                foodTableBody = document.getElementById('food-table-body');
                 foodTableBody.innerHTML += `
                         <tr>
                             <td>${foodLabel}</td>
@@ -29,7 +37,77 @@ window.addEventListener("load", () => {
                             <td>${label}</td>
                         </tr>
                 `;
-            }
+            } //for
+
+            searchBarcode.value = '';
+
+
+            // console.log(servingQuantity);
         });
     });
+
+    const addBtnClick = document.getElementById("add-btn");
+
+    let foodCalTotal = 0;
+    let foodFatTotal = 0;
+    let foodCarbsTotal = 0;
+    let servingQuantity = 0;
+
+    addBtnClick.addEventListener('click', (e) => {
+        // Prevent form from auto-submitting
+        e.preventDefault();
+
+        let foodCal = hit.food.nutrients.ENERC_KCAL;
+        let foodFat = hit.food.nutrients.FAT;
+        let foodCarbs = hit.food.nutrients.CHOCDF;
+
+        servingQuantity = document.getElementById("amount").value;
+
+        let foodCalAdd = Math.round(foodCal * servingQuantity);
+        let foodFatAdd = (foodFat * servingQuantity);
+        let foodCarbsAdd = (foodCarbs * servingQuantity);
+
+        let foodLabelAdd = hit.food.label;
+
+
+        const totalTableBody = document.getElementById('total-table-body');
+        totalTableBody.innerHTML += `
+                    <tr>
+                        <td>${foodLabelAdd}</td>
+                        <td>${foodCalAdd} kcal</td>
+                        <td>${foodFatAdd.toFixed(2)} g</td>
+                        <td>${foodCarbsAdd.toFixed(2)} g</td>
+                    </tr>
+            `;
+        foodCalTotal += foodCalAdd;
+        foodFatTotal += foodFatAdd;
+        foodCarbsTotal += foodCarbsAdd;
+
+        const totalTableFoot = document.getElementById('total-table-foot');
+        totalTableFoot.innerHTML = `
+                        <tr>
+                            <td>Total</td>
+                            <td>${foodCalTotal} kcal</td>
+                            <td>${foodFatTotal.toFixed(2)} g</td>
+                            <td>${foodCarbsTotal.toFixed(2)} g</td>
+                        </tr>
+                    `;
+
+        // servingQuantity.value = 0;
+        console.log(servingQuantity);
+
+        foodCalAdd = null;
+        foodFatAdd = null;
+        foodCarbsAdd = null;
+
+        servingQuantity = document.getElementById("amount").value;
+        searchBarcode = '';
+
+
+        foodTableBody.innerHTML = ``;
+
+
+    }); // click Add btn
+
 });
+
